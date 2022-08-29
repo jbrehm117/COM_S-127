f9acdca61ed24af8b25fc8ed25d0ca077f80270b
jbrehm117 <106644614+jbrehm117@users.noreply.github.com>
2022-08-29 14:40:07 -0500

    Added Zany Text Assignment 1

---
+++ assignment1/zanyText.py
@@ -0,0 +1,48 @@
+# Joseph Brehm             8.29.22
+# Assignment 1
+
+print("Zany Text!")
+print()
+
+print("By: Joseph Brehm")
+print("COM S 127 Section D")
+print()
+
+"""
+# Example 'Zany Text' - gathering input
+print("Once upon a time, there was a <noun>.")
+noun1 = input("noun: ")
+print("It was a <adjective>, <adjective>, <adjective>", noun1)
+adjective1 = input("adjective: ")
+adjective2 = input("adjective: ")
+adjective3 = input("adjective: ")
+print("And, one day, it <past tense action> all over the <noun>!")
+verb1 = input("past tense action: ")
+noun2 = input("noun: ")
+
+# Example 'Zany Text' - printing the final string
+print("Once upon a time, there was a",
+      noun1,
+      ". It was a",
+      adjective1, ",",
+      adjective2, ",",
+      adjective3,
+      noun1,
+      ". And, one day it",
+      verb1,
+      "all over the",
+      noun2, "!")
+"""
+
+# TODO: Make three more 'Zany Texts' like the one above, and have all four run/ print out in order.
+# There should be at least four (4) 'Zany Texts' in a completed assignment.
+
+# Zany Text 1
+print("My dog's name is <dog_name>")
+dogname = input("dog's name?")
+print("She is <color> and likes to <verb>")
+color = input("color?")
+verb = input("verb?")
+print("One day <dog_name> decided to track mud all through the house,", end=" ")
+print("his/her owners were very <descriptive word>")
+descriptive_word = input("descriptive word?")
