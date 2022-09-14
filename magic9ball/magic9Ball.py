fc0f109c46216274abfe85bcc7c2fd4c61dfdc90
jbrehm117 <106644614+jbrehm117@users.noreply.github.com>
2022-09-13 22:04:11 -0500

    magic 9 ball assignment

--- magic9ball/magic9Ball.py
+++ magic9ball/magic9Ball.py
@@ -48,27 +48,37 @@ else:
     print("ERROR: I did not understand your input... Please try again...")
 
 # Prediction
+if choice == "p":
+    question = input("What is your question?: ")
+    selection_value = len(question) % 10
+    print("As far as", question,
+          "is concerned, I predict: ", end="")
+    if selection_value == 0 or 1 or 2 or 3 or 4 or 5 or 6 or 7 or 8 or 9:
+        if selection_value == 0:
+            print("Yes, clearly.")
+        elif selection_value == 1:
+            print("No, I don't think so.")
+        elif selection_value == 2:
+            print("thats an interesting question.")
+        elif selection_value == 3:
+            print("Let me think some more.")
+        elif selection_value == 4:
+            print("Whats that again?")
+        elif selection_value == 5:
+            print("certainly")
+        elif selection_value == 6:
+            print("No, thats really not a good idea.")
+        elif selection_value == 7:
+            print("Try another question.")
+        elif selection_value == 8:
+            print("Dumb question, try again.")
+        elif selection_value == 9:
+            print("DUHHHHHHH, NO.")
+        else:
+            print()
+    else:
+        print("ERROR: selection_value cannot be above 9")
 
-
-# 'p' option tasks ----------------------------------------------------------------------------------------------------------------------------
-# TODO: inside the 'p' section, take in input from the user asking them "What is your question?: " or something similar (use your imagination)
-# TODO: print a string stating print("As far as '", question, "' is concerned, I predict: ") or something similar (use your imagination)
-# TODO: use then length of the question string the user input to generate a 'prediction selection value' within the following range:
-# from 0 through (<the number of predictions>-1)
-# HINT: there is a built in function to find the range of a string
-# HINT: one of the operators used in the 'c' section of the script will return a value within the range 'from 0 through (<the number of predictions>-1)'
-# when applied to <the length of the question string> on the left side of the operator, and <the number of predictions> on the right side
-# TODO: write at least ten (10) 'predictions' and use conditional logic to select which one to print after the 'prediction selection value'
-# from above is calculated (use your imagination)
-# TODO: if the computation to produce a 'prediction selection value' creates a value outside the range 'from 0 through (<the number of predictions>-1)'
-# then have the script print an error message to that effect. NOTE: If the calculation of a 'prediction selection value' is done properly, this
-# error message should **never** be seen.
-# HINT: if you write ten (10) 'predictions,' then <the number of predictions> will be 10. This means that the range of the 'prediction
-# selection value' will be between 0 and 9. After selecting a value between 0 and 9 by way of the length of the 'question' string, one of
-# the operators from the 'c' option, and <the number of predictions>, you will print out a 'prediction' corresponding to this value.
-# ---------------------------------------------------------------------------------------------------------------------------------------------
-
-# 'q' option tasks ----------------------------------------------------------------------------------------------------------------------------
-# TODO: inside the 'q' option, print out a message stating "Maybe next time...", or "Goodbye!", or something else to that effect
-# (use your imagination)
-# ---------------------------------------------------------------------------------------------------------------------------------------------
+# Quit
+if choice == "q":
+    print("Oh darn...see ya later I guess :)")
