96a600a4e0fec6b4548d9372db827ad9f90c1235
jbrehm117 <106644614+jbrehm117@users.noreply.github.com>
2022-10-18 22:43:33 -0500

    Create exercise1.py

---
+++ jcbrehmLabWeek9/exercise1.py
@@ -0,0 +1,23 @@
+#Joseph Brehm       10.18.22
+#Lab Week 9         Exercise 1
+
+import random
+
+x = int(input("Input an integer for A: "))
+y= int(input("input an integer for B: "))
+
+
+def main(x, y):
+    list = []
+    for i in range(x):
+        randNumber = random.randint(x, x+y)
+        list.append(randNumber)
+    print(list)
+    multiples = []
+    for i in list:
+        if i % y == 0:
+            multiples.append(i)
+    return multiples
+
+returnedValues = main(x, y)
+print(returnedValues)
